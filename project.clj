c12e0d9bae7fadc3039f8303dd7d139a2beb25f4
Carlo Sciolla <carlo.sciolla@gmail.com>
2011-11-06 00:26:16 +0100

    Initial version

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject akismet "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [clj-http "0.2.3"]]
+  :repl-options [:init nil :caught clj-stacktrace.repl/pst+])
