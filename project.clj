7398566192c68e7ad2443c63a579aa1a2b41295a
Carlo Sciolla <carlo.sciolla@gmail.com>
2011-11-06 17:04:35 +0100

    Blog now as a dynamic var

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject akismet "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [clj-http "0.2.3"]]
+                 [clj-http "0.2.3"]
+                 [http.async.client "0.4.0-SNAPSHOT"]]
   :repl-options [:init nil :caught clj-stacktrace.repl/pst+])
